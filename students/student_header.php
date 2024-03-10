b7132d30091220b5fc84768d72ef378f451b315c
Rishikdk <rishikhadk6180@gmail.com>
2024-03-10 22:07:21 +0545

    change css of header

--- students/student_header.php
+++ students/student_header.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-include '../database/db_connect.php'; 
+include '../database/db_connect.php';
 
-if(isset($_SESSION['username'])) {
+if (isset($_SESSION['username'])) {
     $username = $_SESSION['username'];
     $sql = "SELECT name, profile_picture FROM learner WHERE email = '$username'";
     $result = $conn->query($sql);
@@ -11,54 +11,58 @@ if(isset($_SESSION['username'])) {
         $name = $row['name'];
         $profile = $row['profile_picture'];
     }
-} 
+}
 
 $userNameDisplay = isset($name) ? $name : 'User';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/admin/admin_header.css">
 </head>
+
 <body>
-<header class="header">  
-    <section class="sec">
-        <div class="logo-container">
-            <a href="home.php">
-            <img src="../images/logo.png" alt="Vidya Vriddhi" class="logo-img">
-            </a>
+    <header class="header">
+        <section class="sec">
+            <div class="logo-container">
+                <a href="home.php">
+                    <img src="../images/logo.png" alt="Vidya Vriddhi" class="logo-img">
+                </a>
+            </div>
+            <form action="" method="POST" class="form">
+                <input type="text" placeholder="search course">
+                <button type="submit" class="fas fa-search" name="search_box"></button>
+            </form>
+            <div class="icons">
+                <div id="menu_btn" class="fas fa-bars"></div>
+                <div id="search_btn" class="fas fa-search"></div>
+                <div id="user_btn" class="fas fa-user"></div>
+            </div>
+        </section>
+    </header>
+    <div class="side-bar">
+        <div class="close-side-bar">
+            <i class="fas fa-times"></i>
         </div>
-        <form action="" method="POST" class="form">
-            <input type="text" placeholder="search course">
-            <button type="submit" class= "fas fa-search" name="search_box"></button>
-        </form>
-        <div class ="icons">
-            <div id ="menu_btn" class="fas fa-bars"></div>
-            <div id ="search_btn" class="fas fa-search"></div>
-            <div id ="user_btn" class="fas fa-user"></div>
+        <div class="profile">
+            <img src="../uploads/profile/<?php echo $profile; ?>" alt="Profile Picture">
+            <h3>
+                <?php echo $userNameDisplay; ?>
+            </h3>
+            <span>student</span>
+            <a href="profile.php" class="btn">View Profile</a>
         </div>
-    </section>
-</header>
-<div class="side-bar">
-    <div class="close-side-bar">
-       <i class="fas fa-times"></i>
+        <nav class="navbar">
+            <a href="home.php"><i class="fas fa-home"></i><span>home</span></a>
+            <a href="about.php"><i class="fas fa-question"></i><span>about us</span></a>
+            <a href="courses.php"><i class="fas fa-graduation-cap"></i><span>courses</span></a>
+            <a href="teachers.php"><i class="fas fa-chalkboard-user"></i><span>teachers</span></a>
+            <a href="contact.php"><i class="fas fa-headset"></i><span>contact us</span></a>
+        </nav>
     </div>
-    <div class="profile">
-    <img src="../uploads/profile/<?php echo $profile; ?>" alt="Profile Picture">
-        <h3><?php echo $userNameDisplay; ?></h3>
-        <span>student</span>
-        <a href="profile.php" class="btn">View Profile</a>
-     </div>
-    <nav class="navbar">
-        <a href="home.php"><i class="fas fa-home"></i><span>home</span></a>
-        <a href="about.php"><i class="fas fa-question"></i><span>about us</span></a>
-        <a href="courses.php"><i class="fas fa-graduation-cap"></i><span>courses</span></a>
-        <a href="teachers.php"><i class="fas fa-chalkboard-user"></i><span>teachers</span></a>
-        <a href="contact.php"><i class="fas fa-headset"></i><span>contact us</span></a>
-     </nav>
-</div>
