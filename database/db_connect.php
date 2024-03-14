f1be910d4806a024c14756cb9412b6a02b74a6ab
Rishi Khadka <120002587+Rishikdk@users.noreply.github.com>
2024-03-14 16:58:23 +0545

    fixed

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,7 +1,19 @@
 <?php
-$servername = "localhost";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "web";
+
+// $conn = new mysqli($servername, $username, $password, $dbname);
+
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+
+//}
+
+$servername = "localhost:3300";
 $username = "root";
-$password = "";
+$password = "root";
 $dbname = "web";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
